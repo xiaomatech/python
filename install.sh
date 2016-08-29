cf310bbc0406756be6469595d2323ebdd4c74945
xianhuawei <xianhuawei.zhcn@qq.com>
2016-08-30 01:55:29 +0800

    install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo yum install -y git redis python-pip python-simplejson python-requests MySQL-python
 sudo pip install --upgrade pip
-sudo pip install -r req.txt
+sudo pip install -r requirements.txt
 
 service redis start
 
